80956cb1844f2aa51b8d765cef49d34efe09a04a
691513006@qq.com <lwq946777>
2021-09-07 01:35:16 +0800

    The obtained data is stored in the database

---
+++ db/migrate/20210906172615_create_vaccination_forms.rb
@@ -0,0 +1,14 @@
+class CreateVaccinationForms < ActiveRecord::Migration[6.1]
+  def change
+    create_table :vaccination_forms do |t|
+      t.string :company
+      t.string :number
+      t.string :nonumber
+      t.string :name
+      t.integer :phone
+      t.integer :response_id
+
+      t.timestamps
+    end
+  end
+end
