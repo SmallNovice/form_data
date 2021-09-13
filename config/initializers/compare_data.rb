28763c510002c993fa8886fe16634896b74432ae
691513006@qq.com <lwq946777>
2021-09-14 01:25:42 +0800

    When the rails service starts, the data to be deleted is updated synchronously

--- config/initializers/compare_data.rb
+++ config/initializers/compare_data.rb
@@ -1,17 +1,38 @@
 class CompareData
-    flag = true
+    flag_create_and_update = true
     page = 0
+    array = []
+    array_index = 0
+    len = VaccinationForm.last.id
+    data_index = 1
+    flag_delete = true
+    response_id = []
 
-    while flag
+    while flag_create_and_update
       responses = SkylarkService.new.query_form_responses(VaccinationForm.form_id,page += 1)
 
-      flag = false if responses.headers[:x_slp_total_pages].to_i == page
+      flag_create_and_update = false if responses.headers[:x_slp_total_pages].to_i == page
 
       ActiveRecord::Base.transaction do
         JSON.parse(responses).each do |response|
-            VaccinationForm.upsert(response)
+          array[array_index] = response['id']
+          array_index += 1
+          VaccinationForm.upsert(response)
         end
       end
+    end
+
+    while flag_delete
+      flag_delete = false if data_index == len
+      if vacc = VaccinationForm.find_by(id: data_index)
+
+        response_id = [vacc.response_id]
+
+        if (response_id & array).empty?
+          vacc.destroy
+        end
+      end
+      data_index += 1
     end
 end
 
