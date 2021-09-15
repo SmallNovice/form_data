3c23e46089fc110272fa87334e4e95ec2ea23c92
SmallNovice <691513006@qq.com>
2021-09-15 21:12:06 +0800

    修改application.rb

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,5 @@ module FormData
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.eager_load_paths += %W(#{config.root}/lib/skylark/tools.rb)
   end
 end
